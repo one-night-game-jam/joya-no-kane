881c10b13125429d5d3566b623e98404004007c5
Shota HAMADA <maku693@outlook.com>
2018-11-23 19:08:02 +0900

    Add life to Shumoku

--- Assets/Scripts/Players/ShumokuBehaviour.cs
+++ Assets/Scripts/Players/ShumokuBehaviour.cs
@@ -1,13 +1,58 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using Zenject;
 
 namespace Players
 {
     public class ShumokuBehaviour : MonoBehaviour
     {
+        [SerializeField] float lifeReduction;
+        private FloatReactiveProperty _life = new FloatReactiveProperty(1.0F);
+        public IObservable<float> life => _life;
+
+        [Inject]
+        KaneSpawner kaneSpawner;
+
+        void Start()
+        {
+            kaneSpawner.Spawned
+                .SelectMany(x => x.Select(k => k.IsDeadAsObservable()).Merge())
+                .Where(x => x)
+                .First()
+                .SelectMany(_ => this.UpdateAsObservable())
+                .Subscribe(_ => { UpdateLife(); })
+                .AddTo(this);
+        }
+
         void OnTriggerEnter(Collider other)
         {
             var hittable = other.GetComponent<IHittable>();
-            hittable?.Hit();
+
+            if (hittable == null)
+            {
+                return;
+            }
+            hittable.Hit();
+
+            MaximizeLife();
+        }
+
+        void MaximizeLife()
+        {
+            _life.Value = 1.0F;
+        }
+
+        void UpdateLife()
+        {
+            _life.Value -= lifeReduction * Time.deltaTime;
+            if (_life.Value < 0)
+            {
+                _life.Value = 0;
+            }
         }
     }
 }
