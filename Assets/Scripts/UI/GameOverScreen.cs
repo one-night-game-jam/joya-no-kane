89be9dd71f806614456ef58208232a15dd76ae74
Shota HAMADA <maku693@outlook.com>
2018-11-23 21:11:51 +0900

    Fix the bug where the game over screen is shown when the game is cleared

--- Assets/Scripts/UI/GameOverScreen.cs
+++ Assets/Scripts/UI/GameOverScreen.cs
@@ -3,6 +3,7 @@ using UniRx;
 using UnityEngine;
 using Zenject;
 
+using GameManagers;
 using Players;
 
 namespace UI
@@ -10,12 +11,15 @@ namespace UI
     class GameOverScreen : MonoBehaviour
     {
         [Inject] ShumokuBehaviour shumoku;
+        [Inject] KaneCounter kaneCounter;
+
 
         void Start()
         {
             gameObject.SetActive(false);
 
             shumoku.Dead
+                .TakeUntil(kaneCounter.NoRemainingKane)
                 .Subscribe(_ => gameObject.SetActive(true))
                 .AddTo(this);
         }
