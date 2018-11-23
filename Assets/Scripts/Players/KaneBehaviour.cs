cee3a2b40bcdc955880c8547e2643321ea301ea7
Shota HAMADA <maku693@outlook.com>
2018-11-23 16:05:59 +0900

    Add TimeCounter

--- Assets/Scripts/Players/KaneBehaviour.cs
+++ Assets/Scripts/Players/KaneBehaviour.cs
@@ -1,8 +1,14 @@
-﻿using UnityEngine;
+﻿using System;
+using UniRx;
+using UnityEngine;
 
 namespace Players
 {
     public class KaneBehaviour : MonoBehaviour
     {
+        public IObservable<bool> IsDeadAsObservable()
+        {
+            return Observable.Return(true);
+        }
     }
 }
