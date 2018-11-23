b6357eff7a80dc9d12c44f1b0ff10fdee870b565
Shota HAMADA <maku693@outlook.com>
2018-11-23 21:47:43 +0900

    Wait before enable reloading of the game

--- Assets/Scripts/GameManagers/Reloader.cs
+++ Assets/Scripts/GameManagers/Reloader.cs
@@ -1,4 +1,6 @@
+using System;
 using UniRx;
+using UniRx.Async;
 using UniRx.Triggers;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -12,12 +14,15 @@ namespace GameManagers
     public class Reloader : MonoBehaviour
     {
         [SerializeField] private string sceneName;
+        [SerializeField] private float waitToGetInput;
 
         [Inject] private ShumokuBehaviour shumoku;
         [Inject] private KaneCounter kaneCounter;
 
-        private void Start()
+        private async UniTask Start()
         {
+            await UniTask.Delay(TimeSpan.FromSeconds(waitToGetInput));
+
             var finished = Observable.Merge(shumoku.Dead, kaneCounter.NoRemainingKane);
 
             GetComponent<IInputEventProvider>()
